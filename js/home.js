7c35a95513c264694037ecca8546a932c91671d0
Frank Fang <frankfang2014@hotmail.com>
2018-06-13 17:29:56 -0700

    counterpage done

--- js/home.js
+++ js/home.js
@@ -12,4 +12,52 @@ $(document).ready(function() {
         }
       
       );
+// counter
+
+
+
+let soldCounter = 203800;
+    let lastCounterNumberArray = ["2","0","3","8","0","0"]; 
+    let date = new Date().toLocaleDateString().toString();
+    // date = date.replace(/\\/g,"");
+    console.log(date);
+    setInterval(function(){
+        const ranNum = (Math.floor(Math.random()*7)+2);
+        soldCounter +=ranNum;
+        const conuterStringArray = soldCounter.toString().split("");
+        let counterNumberText = "";
+        for(let i=0;i<conuterStringArray.length;i++)
+        {
+            // if(i==conuterStringArray.length-1)
+            if(conuterStringArray[i]!=lastCounterNumberArray[i])
+            {
+                counterNumberText += `<span class="counter-digit-box "><b class="digit-jump">${conuterStringArray[i]}</b></span>`;
+            }
+            else{
+                counterNumberText += `<span class="counter-digit-box"><b>${conuterStringArray[i]}</b></span>`;
+            }
+            
+        }
+        lastCounterNumberArray = conuterStringArray;
+        $('#counter-box').html(counterNumberText);
+
+    },4000);
+
+
+// date
+
+var currentdate = new Date(); 
+    var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + "  "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+$('.date').html(datetime);
+
+
 });
+
+
+
