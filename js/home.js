be820e828a572a648c5158b7639849136e409dd0
Frank Fang <frankfang2014@hotmail.com>
2018-06-14 11:58:14 -0700

    nav reponsive done

--- js/home.js
+++ js/home.js
@@ -97,9 +97,54 @@ $('.date').html(datetime);
         lastCounterNumberArray = conuterStringArray;
         $('#counter-box').html(counterNumberText); 
     }
+// hamburger
+// const body = document.body;
+// const btn = document.querySelector('.menu');
+
+// btn.addEventListener('click',_ =>{
+
+// body.classList.toggle('show');
 
 
+// });
+$('.menu').click(function(){
 
+    $('.nav-right').slideToggle();
+
+});
+
+var Menu = {
+  
+    el: {
+      ham: $('.menu'),
+      menuTop: $('.menu-top'),
+      menuMiddle: $('.menu-middle'),
+      menuBottom: $('.menu-bottom')
+    },
+    
+    init: function() {
+      Menu.bindUIactions();
+    },
+    
+    bindUIactions: function() {
+      Menu.el.ham
+          .on(
+            'click',
+          function(event) {
+          Menu.activateMenu(event);
+          event.preventDefault();
+        }
+      );
+    },
+    
+    activateMenu: function() {
+      Menu.el.menuTop.toggleClass('menu-top-click');
+      Menu.el.menuMiddle.toggleClass('menu-middle-click');
+      Menu.el.menuBottom.toggleClass('menu-bottom-click'); 
+    }
+  };
+  
+  Menu.init();
 
 });
 
